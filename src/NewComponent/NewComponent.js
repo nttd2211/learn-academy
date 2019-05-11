a8b37ec0e58277dec8bb188a0b53e22781c06940
Nguyen Thi Thuy Duyen <nttd2211@gmail.com>
2019-05-11 18:25:09 +0700

    NewComponent

---
+++ src/NewComponent/NewComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './NewComponent.css';
+
+class NewComponent extends Component {
+    render() {
+        // const styleComponent = 
+        // {
+        //     padding: '20x',
+        //     color: 'green',
+        //     fontSize: '70px',
+        // }
+        return (
+            <div>
+                <h3 id="ChangeStyle" >This is my website</h3>
+            </div>
+        );
+    }
+}
+
+export default NewComponent;
